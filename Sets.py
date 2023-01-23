b6b9aadf9add5ebe0b9aa7e154a527ef0b9e2f5f
jlucas577 <lucas-martinsbr@hotmail.com>
2023-01-23 17:34:24 -0300

    Including sets

---
+++ Sets.py
@@ -0,0 +1,6 @@
+fruits = {"avocato", "tomato", "banana"}
+vegetables = {"beetroot", "carrot", "tomato"}
+
+print("Union: " + str(fruits | vegetables))
+print("Intersection: " + str(fruits & vegetables))
+print("Difference: " + str(fruits - vegetables))
